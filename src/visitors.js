f8802c7efd3a0e950c8c1d8eb9e00a93245e8314
mon0macal7 <monhdz07@gmail.com>
2021-12-16 20:56:13 -0600

    iterando empresas

--- src/visitors.js
+++ src/visitors.js
@@ -4,8 +4,29 @@ fetch("../data/loop.json")
   .then((data) => iterarEmpresa(data))
   .catch((error) => console.log(error));
 
+//Función iterar empresas
+let empresa = document.getElementById("dropdown");
+
 let iterarEmpresa = (data) => {
   console.log(data.LoopBuilding);
-  for (const empresa of data.LoopBuilding) {
+
+  for (const oficina of data.LoopBuilding) {
+    console.log(oficina.Empresa);
+    console.log();
+    empresa.innerHTML += `<select onclick= "dropdown" class="empres-select">
+    <option value="Sourceful">${oficina.Empresa}</option>
+             </select>`;
+  }
+};
+//Funcion iterar personal
+let personal = document.getElementById("dropdown2");
+
+let iterarPersonal = (data) => {
+  console.log(data.LoopBuilding);
+
+  for (const encargado in LoopBuilding) {
+    if (Object.hasOwnProperty.call(object, encargado)) {
+      const element = object[encargado];
+    }
   }
 };
