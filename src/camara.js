5c9fb31d4f265d66397e965b9177cd9fa7d36ae0
mon0macal7 <monhdz07@gmail.com>
2021-12-28 13:24:47 -0600

    implementando base64 img

--- src/camara.js
+++ src/camara.js
@@ -1,30 +1,36 @@
+const registro = document.getElementById("btn-siguiente");
+registro.addEventListener("click", () => {
+  document.getElementById("pantalla1-registro").hidden = true;
+  document.getElementById("pantalla2-foto").hidden = false;
+  const video = document.getElementById("video");
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
+    .then((stream) => {
+      video.srcObject = stream;
+      console.log(stream);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  document.getElementById("tomarFoto").addEventListener("click", () => {
+    getImg();
+  });
 
-
-const registro = document.getElementById('btn-siguiente')
-      registro.addEventListener('click', () => {
-        document.getElementById('pantalla1-registro').hidden = true;
-        document.getElementById('pantalla2-foto').hidden = false;
-         const video = document.getElementById('video')
-      navigator.mediaDevices.getUserMedia({ video: true })
-        .then(
-          (stream) => {
-            video.srcObject = stream;
-            console.log(stream)
-          }
-        )
-        .catch((error) => {
-          console.log(error)
-        })
-      document.getElementById("tomarFoto").addEventListener("click", () => {
-        tomarFoto()
-      })
-
-      function tomarFoto() {
-        const canvas = document.getElementById("canvas")
-        let ctx = canvas.getContext('2d');
-        ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight)
-      
-    
-}
-      })
-    
+  function getImg(video) {
+    const canvas = document.getElementById("canvas");
+    let ctx = canvas.getContext("2d");
+    ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+    let dataURL = canvas.toDataURL();
+    return dataURL;
+  }
+  let base64 = getImg(document.getElementById("video"));
+  console.log(base64);
+  formulario.foto = base64;
+  formulario.date = ctx.drawImage(
+    video,
+    0,
+    0,
+    video.videoWidth,
+    video.videoHeight
+  );
+});
