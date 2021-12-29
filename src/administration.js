a7c521dcf1a0de74dee400f57bbc108782401514
mon0macal7 <monhdz07@gmail.com>
2021-12-29 17:44:21 -0600

    status anterior

--- src/administration.js
+++ src/administration.js
@@ -1,1 +1,50 @@
+/*const onGetData = (callback) =>
+  db.collection("regVisitantes").onSnapshot(callback);
 
+export async function pintarDatos() {
+  onGetData((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+      let visit = doc.data();
+      visit.id = doc.id;
+      console.log(visit);
+      traerData.push(visit);
+    });
+
+    // Se le pasan los datos de firebase a la variable arrayVisiantes
+    arrayVisitantes = traerData;
+    console.log(arrayVisitantes);
+    pintarRegistro();
+  });
+}
+
+// Se crea la funcion que servirá para pintar a los vistantes dentro de la tabla
+
+let pintarRegistro = () => {
+  console.log(arrayVisitantes.length);
+  let tabla = arrayVisitantes[0];
+  console.log(tabla.nombre);
+
+  // Se hace una iteracion con for of
+  for (const tabla of arrayVisitantes) {
+    document.getElementById("tabla").innerHTML += `
+  <tr>
+        <td>${tabla.nombre}</td>
+          <td>${tabla.apellido}</td>
+          <td>${tabla.correo}</td>
+          <td>${tabla.empresa}</td>
+          <td>${tabla.motivo}</td>
+          <td>${tabla.cita}</td>
+          <td>${tabla.encargado}</td>
+        </tr>
+  `;
+  }
+};
+*/
+// const prueba = { ciela: "vania" };
+
+// db.collection("visitantes")
+//   .doc()
+//   .set(prueba)
+//   .then(() => {
+//     console.log("prueba");
+//   });
--- src/visitors.js
+++ src/visitors.js
@@ -39,10 +39,6 @@ document.getElementById("tomarFoto").addEventListener("click", () => {
   console.log(keepImg);
 });
 
-//Arreglos para añadir los registros
-let traerData = [];
-let arrayVisitantes = [];
-
 // Se enciende la conexión con firestore
 const db = firebase.firestore();
 // // Aqui empieza la tabla
@@ -65,48 +61,6 @@ const db = firebase.firestore();
 //     </tr>`
 //   })
 
-const onGetData = (callback) =>
-  db.collection("regVisitantes").onSnapshot(callback);
-
-export async function pintarDatos() {
-  onGetData((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-      let visit = doc.data();
-      visit.id = doc.id;
-      console.log(visit);
-      traerData.push(visit);
-    });
-
-    // Se le pasan los datos de firebase a la variable arrayVisiantes
-    arrayVisitantes = traerData;
-    console.log(arrayVisitantes);
-    pintarRegistro();
-  });
-}
-
-// Se crea la funcion que servirá para pintar a los vistantes dentro de la tabla
-
-let pintarRegistro = () => {
-  console.log(arrayVisitantes.length);
-  let tabla = arrayVisitantes[0];
-  console.log(tabla.nombre);
-
-  // Se hace una iteracion con for of
-  for (const tabla of arrayVisitantes) {
-    document.getElementById("tabla").innerHTML += `
-  <tr>
-        <td>${tabla.nombre}</td>
-          <td>${tabla.apellido}</td>
-          <td>${tabla.correo}</td>
-          <td>${tabla.empresa}</td>
-          <td>${tabla.motivo}</td>
-          <td>${tabla.cita}</td>
-          <td>${tabla.encargado}</td>
-        </tr>
-  `;
-  }
-};
-
 // const prueba = { ciela: "vania" };
 
 // db.collection("visitantes")
@@ -128,61 +82,20 @@ registroVisitantes.addEventListener("click", async (e) => {
     motivo: document.getElementById("dropdown1").value,
     cita: document.getElementById("cita").value,
     encargado: document.getElementById("dropdown2").value,
-    foto: document.getElementById("tomarFoto").value,
   };
   console.log(formulario);
-  await guardarObj(formulario);
+  await guardarObj(formulario); //este await no funciona
 });
 
 const guardarObj = (formulario) => {
   console.log(formulario);
   db.collection("registro").doc().set(formulario);
 
-  window.location.href = "./index.html";
+  // window.location.href = "./index.html";
 };
 
 //firebase.firestore();
 console.log(db);
-
-/*const registrar = "../data/loop.json";
-console.log(registrar);
-let arrayEmpresas = [];
-let arrayPersonal = [];
-
-const traerData = () => {
-  fetch(registrar)
-    .then((response) => response.json(response))
-    .then((data) => {
-      arrayEmpresas.push(data);
-      console.log(arrayEmpresas);
-      iterarEmpresas(arrayEmpresas[0].LoopBuilding);
-    });
-};
-
-// iterar empresas
-const iterarEmpresas = (arraydeEmpresas) => {
-  for (const [index, empresa] of arraydeEmpresas.entries()) {
-    //console.log(empresa);
-
-    let empresas = document.getElementById("dropdown");
-    empresas.innerHTML += `
-        <option onclick= "selectEmpresa('${index}')">${empresa.Empresa}</option>
-                `;
-    //console.log(empresas);
-  }
-};
-let selectEmpresa = (index) => {
-  console.log(arrayEmpresas[0].LoopBuilding[index].Personal);
-  let empleados = document.getElementById("dropdown2");
-  empleados.innerHTML = "";
-  for (const empleado of arrayEmpresas[0].LoopBuilding[index].Personal) {
-    console.log(empleado);
-
-    empleados.innerHTML += `<option>${empleado} </option>`;
-    //console.log(empleados);
-  }
-};
-traerData();*/
 
 /*const save = () => {
   let buttonKeep = document.getElementById("enviar");
