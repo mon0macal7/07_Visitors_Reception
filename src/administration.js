16b9a3568f0d0d4663fabd6d348ccde9f8d332dc
getzemani18 <amandaalvarado0129@gmail.com>
2021-12-29 22:41:38 -0600

    .

--- src/administration.js
+++ src/administration.js
@@ -15,8 +15,27 @@ db.collection("registro").onSnapshot((querySnapshot) => {
       <td>${doc.data().motivo}</td>
       <td>${doc.data().cita}</td>
       <td>${doc.data().encargado}</td>
-
-    </tr>`
+     <td> <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+ <i class="bi bi-image-fill"></i>
+</button></td>
+    </tr>
+    <div class="modal" tabindex="-1">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <p>Modal body text goes here.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>`
   });
 });
       // <td>${doc.data().fecha.toDate()}</td>
