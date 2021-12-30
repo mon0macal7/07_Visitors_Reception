4b68ffc4826d01c1ce1e5d448f824267ccaefeea
getzemani18 <amandaalvarado0129@gmail.com>
2021-12-29 23:30:17 -0600

    .

--- src/administration.js
+++ src/administration.js
@@ -27,15 +27,16 @@ db.collection("registro").onSnapshot((querySnapshot) => {
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Foto</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      <div class="modal-body">
-       <img  src="${doc.data().foto}">
-      </div>
+      <div class="modal-dialog modal-fullscreen-sm-down">
+  <img  src="${doc.data().foto}">
+</div>
+
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        
       </div>
     </div>
   </div>
